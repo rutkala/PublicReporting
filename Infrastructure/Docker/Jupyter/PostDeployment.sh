640dec9cc8288382455ea49b7e94a20047bf5938
rutkala <r.utkala@gmail.com>
2024-12-07 00:36:56 +0100

    Superset

--- Infrastructure/Docker/Jupyter/PostDeployment.sh
+++ Infrastructure/Docker/Jupyter/PostDeployment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # JAR URLs
-NESSIE_JAR_URL="https://repo1.maven.org/maven2/org/projectnessie/nessie-spark-extensions-3.5_2.12/0.95.0/nessie-spark-extensions-3.5_2.12-0.95.0.jar"
+NESSIE_JAR_URL="https://repo1.maven.org/maven2/org/projectnessie/nessie-integrations/nessie-spark-extensions-3.5_2.12/0.95.0/nessie-spark-extensions-3.5_2.12-0.95.0.jar"
 ICEBERG_JAR_URL="https://repo1.maven.org/maven2/org/apache/iceberg/iceberg-spark-runtime-3.5_2.12/1.5.2/iceberg-spark-runtime-3.5_2.12-1.5.2.jar"
 AWS_SDK_JAR_URL="https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/1.11.900/aws-java-sdk-bundle-1.11.900.jar"
 HADOOP_AWS_JAR_URL="https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/3.3.1/hadoop-aws-3.3.1.jar"
