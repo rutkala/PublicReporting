152614446fd691f03051cbd510fefe3aedf62046
rutkala <82494338+rutkala@users.noreply.github.com>
2025-09-10 11:41:56 +0200

    Update DeployMinIO.sh

--- Infrastructure/Lakehouse/MinIO/DeployMinIO.sh
+++ Infrastructure/Lakehouse/MinIO/DeployMinIO.sh
@@ -7,7 +7,7 @@ echo "Starting PreDeployment..."
 ./PreDeployment.sh
 
 echo "Starting Docker Compose Up..."
-docker compose -f /opt/publicreporting/Infrastructure/Lakehouse/docker-compose.yml up minio -d
+docker compose -f /opt/publicreporting/Infrastructure/Lakehouse/docker-compose.yml up minio mc -d
 
 echo "Starting PostDeployment..."
 ./PostDeployment.sh
