7c7640612803369c915080182a200699ab9e4077
rutkala <82494338+rutkala@users.noreply.github.com>
2025-09-12 21:43:50 +0200

    Update PreDeployment.sh

--- Infrastructure/Lakehouse/nginx/PreDeployment.sh
+++ Infrastructure/Lakehouse/nginx/PreDeployment.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
+set -e
 NGINX_CONF_DIR="./nginx/conf.d"
 CERTS_DIR="./nginx/certs"
-DOMAIN="minio.open-reporting.dev"
 
+echo "[PreDeployment] Creating nginx config and certs folders if not exist..."
 mkdir -p "$NGINX_CONF_DIR" "$CERTS_DIR"
 
-# Step 1: Create temporary HTTP-only config for nginx
-cat > "$NGINX_CONF_DIR/minio.conf" <<EOF
-server {
-    server_name $DOMAIN;
-
-    listen 80;
-    listen [::]:80;
-
-    location / {
-        proxy_pass http://minio:9001;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-    }
-}
-EOF
-
-echo "[PreDeployment] Temporary HTTP nginx config created for $DOMAIN"
+echo "[PreDeployment] Applying HTTP-only config to enable certbot validation..."
+cp "$NGINX_CONF_DIR/minio-http.conf" "$NGINX_CONF_DIR/minio.conf"
