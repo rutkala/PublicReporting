bb944c0d30b5ab8d2999004147ec56ce02dc327b
rutkala <82494338+rutkala@users.noreply.github.com>
2025-09-12 22:26:38 +0200

    Update PreDeployment.sh

--- Infrastructure/Lakehouse/nginx/PreDeployment.sh
+++ Infrastructure/Lakehouse/nginx/PreDeployment.sh
@@ -6,4 +6,4 @@ echo "[PreDeployment] Creating nginx config and certs folders if not exist..."
 mkdir -p "$CERTS_DIR"
 
 echo "[PreDeployment] Applying HTTP-only config to enable certbot validation..."
-cp "$NGINX_CONF_DIR/minio-http.conf" "$NGINX_CONF_DIR/minio.conf"
+cp "./conf.d/minio-http.conf" "./conf.d/minio.conf"
