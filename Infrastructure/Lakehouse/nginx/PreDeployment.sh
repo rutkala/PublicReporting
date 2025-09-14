ea0e0a1b9664cb68542bdeadc3808c5e32e74376
rutkala <82494338+rutkala@users.noreply.github.com>
2025-09-14 13:19:50 +0200

    Update PreDeployment.sh

--- Infrastructure/Lakehouse/nginx/PreDeployment.sh
+++ Infrastructure/Lakehouse/nginx/PreDeployment.sh
@@ -7,8 +7,3 @@ mkdir -p "$CERTS_DIR"
 
 echo "[PreDeployment] Applying HTTP-only config to enable certbot validation..."
 cp "./conf.d/minio-http.conf" "./conf.d/minio.conf"
-
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-  -keyout ./certs/live/storage.open-reporting.dev/privkey.pem \
-  -out ./certs/live/storage.open-reporting.dev/fullchain.pem \
-  -subj "/CN=storage.open-reporting.dev"
