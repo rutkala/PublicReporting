f03f4acf2431ca5a1f0ab8400f887c64a22962ed
rutkala <82494338+rutkala@users.noreply.github.com>
2025-09-17 17:37:38 +0200

    Create superset_config.py

---
+++ Infrastructure/Lakehouse/Superset/superset_config.py
@@ -0,0 +1,9 @@
+# superset_config.py
+
+# Async query transport
+GLOBAL_ASYNC_QUERIES_TRANSPORT = "ws"
+
+# Secure WebSocket endpoint
+GLOBAL_ASYNC_QUERIES_WEBSOCKET_URL = "wss://bi.open-reporting.dev/ws/"
+
+# You can also add other configs here later if needed
